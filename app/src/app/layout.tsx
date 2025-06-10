cf0da32218605d371e0894a7cba3e11e5421ae37
wz73 <williamzheng73@gmail.com>
2025-06-09 21:37:41 -0300

    create next js app

---
+++ app/src/app/layout.tsx
@@ -0,0 +1,26 @@
+import type { Metadata } from "next";
+import { Open_Sans } from "next/font/google";
+import "./globals.css";
+
+const openSans = Open_Sans({
+    variable: "--font-open-sans",
+    subsets: ["latin"]
+});
+
+export const metadata: Metadata = {
+    title: "Satellite",
+    description:
+        "Satellite helps you launch your projects and ideas into orbit, organizing them visually with constellations and detailed missions."
+};
+
+export default function RootLayout({
+    children
+}: Readonly<{
+    children: React.ReactNode;
+}>) {
+    return (
+        <html lang="en">
+            <body className={`${openSans.variable} antialiased`}>{children}</body>
+        </html>
+    );
+}
