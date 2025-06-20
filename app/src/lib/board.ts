89a600bbb0612d6a987585bb8b8811d270c48958
wz73 <williamzheng73@gmail.com>
2025-06-19 23:29:00 -0300

    add board router and board lib

---
+++ app/src/lib/board.ts
@@ -0,0 +1,82 @@
+import { db } from "@/db";
+import { boards, activities } from "@/db/schema";
+import { eq } from "drizzle-orm";
+
+export const BoardLib = {
+    create: async function(workspaceId: number, name: string, backgroundId: number, userId: number) {
+        const [board] = await db
+            .insert(boards)
+            .values({
+                name: name,
+                workspaceId: workspaceId,
+                backgroundId: backgroundId
+            })
+            .returning();
+
+        await db.insert(activities).values({
+            workspaceId: workspaceId,
+            userId: userId,
+            entityId: board.id,
+            entityName: name,
+            entityType: "board",
+            action: "create"
+        });
+
+        return board;
+    },
+
+    update: async function(boardId: number, name: string, backgroundId: number, userId: number) {
+        const [board] = await db.update(boards).set({
+            name: name,
+            backgroundId: backgroundId
+        }).where(eq(boards.id, boardId)).returning();
+
+        await db.insert(activities).values({
+            workspaceId: board.workspaceId,
+            userId: userId,
+            entityId: board.id,
+            entityName: name,
+            entityType: "board",
+            action: "update"
+        });
+    },
+    
+    delete: async function(boardId: number, userId: number) {
+        const [board] = await db.delete(boards).where(eq(boards.id, boardId)).returning();
+
+        await db.insert(activities).values({
+            workspaceId: board.workspaceId,
+            userId: userId,
+            entityId: board.id,
+            entityName: board.name,
+            entityType: "board",
+            action: "delete"
+        });
+    },
+    
+    getById: async function(boardId: number) {
+        return await db
+            .select({
+                id: boards.id,
+                name: boards.name,
+                backgroundId: boards.backgroundId,
+                workspaceId: boards.workspaceId,
+                createdAt: boards.createdAt
+            })
+            .from(boards)
+            .where(eq(boards.id, boardId))
+            .then((rows) => rows[0]);
+    },
+    
+    getAll: async function(workspaceId: number) {
+        return await db
+            .select({
+                id: boards.id,
+                name: boards.name,
+                backgroundId: boards.backgroundId,
+                workspaceId: boards.workspaceId
+            })
+            .from(boards)
+            .where(eq(boards.workspaceId, workspaceId));
+    },
+};
---
+++ app/src/server/routers/board.ts
@@ -0,0 +1,86 @@
+import z from "zod";
+import { TRPCError } from "@trpc/server";
+import { isAuthed } from "../middleware/isAuthed";
+import { publicProcedure, router } from "../trpc";
+import { BoardLib } from "@/lib/board";
+import { MembershipLib } from "@/lib/membership";
+
+export const boardRouter = router({
+    create: publicProcedure
+        .use(isAuthed)
+        .input(
+            z.object({
+                workspaceId: z.number(),
+                name: z.string().min(1),
+                backgroundId: z.number()
+            })
+        )
+        .mutation(async ({ ctx, input }) => {
+            const role = await MembershipLib.getRole(input.workspaceId, ctx.user.id);
+            
+            if (role === "unknown") throw new TRPCError({ code: "NOT_FOUND" });
+            if (role !== "owner" && role !== "admin") throw new TRPCError({ code: "FORBIDDEN" });
+
+            BoardLib.create(input.workspaceId, input.name, input.backgroundId, ctx.user.id);
+        }),
+
+    edit: publicProcedure
+        .use(isAuthed)
+        .input(z.object({
+            id: z.number(),
+            name: z.string().min(1),
+            backgroundId: z.number()
+        }))
+        .mutation(async ({ ctx, input }) => {
+            const board = await BoardLib.getById(input.id);
+            if (!board) throw new TRPCError({ code: "NOT_FOUND" });
+
+            const role = await MembershipLib.getRole(board.workspaceId, ctx.user.id);
+            
+            if (role === "unknown") throw new TRPCError({ code: "NOT_FOUND" });
+            if (role !== "owner" && role !== "admin") throw new TRPCError({ code: "FORBIDDEN" });
+
+            BoardLib.update(input.id, input.name, input.backgroundId, ctx.user.id)
+        }),
+
+    delete: publicProcedure
+        .use(isAuthed)
+        .input(z.object({ id: z.number() }))
+        .mutation(async ({ ctx, input }) => {
+            const board = await BoardLib.getById(input.id);
+            if (!board) throw new TRPCError({ code: "NOT_FOUND" });
+
+            const role = await MembershipLib.getRole(board.workspaceId, ctx.user.id);
+            
+            if (role === "unknown") throw new TRPCError({ code: "NOT_FOUND" });
+            if (role !== "owner" && role !== "admin") throw new TRPCError({ code: "FORBIDDEN" });
+
+            BoardLib.delete(input.id, ctx.user.id);
+        }),
+
+    getById: publicProcedure
+        .use(isAuthed)
+        .input(z.object({ id: z.number() }))
+        .query(async ({ ctx, input }) => {
+            const board = await BoardLib.getById(input.id);
+            if (!board) throw new TRPCError({ code: "NOT_FOUND" });
+
+            const role = await MembershipLib.getRole(board.workspaceId, ctx.user.id);
+            if (role === "unknown") throw new TRPCError({ code: "NOT_FOUND" });
+
+            return board;
+        }),
+
+    getAll: publicProcedure
+        .use(isAuthed)
+        .input(z.object({ workspaceId: z.number() }))
+        .query(async ({ ctx, input }) => {
+            const role = await MembershipLib.getRole(input.workspaceId, ctx.user.id);
+            if (role === "unknown") throw new TRPCError({ code: "NOT_FOUND" });
+
+            const boards = await BoardLib.getAll(input.workspaceId);
+            if (!boards) throw new TRPCError({ code: "NOT_FOUND" });
+
+            return boards;
+        }),
+});
