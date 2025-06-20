f60ccd9f2309c589103db578aebb8a2f35419742
wz73 <williamzheng73@gmail.com>
2025-06-19 23:31:21 -0300

    add remaining routers (activity and board)

--- app/src/server/routers/_app.ts
+++ app/src/server/routers/_app.ts
@@ -1,14 +1,18 @@
 import { router } from "../trpc";
 import { authRouter } from "./auth";
-import { membershipRouter } from "./membership";
 import { userRouter } from "./user";
 import { workspaceRouter } from "./workspace";
+import { membershipRouter } from "./membership";
+import { activityRouter } from "./activity";
+import { boardRouter } from "./board";
 
 export const appRouter = router({
     auth: authRouter,
     user: userRouter,
     workspace: workspaceRouter,
-    membership: membershipRouter
+    membership: membershipRouter,
+    activity: activityRouter,
+    board: boardRouter
 });
 
 export type AppRouter = typeof appRouter;
